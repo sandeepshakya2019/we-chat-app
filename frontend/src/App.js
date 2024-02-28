35f53c4452c2df6237368717d0006091b9bc4715
Sandeep Kuamr <sandeepshakya2015@gmail.com>
2024-02-28 11:30:03 +0530

    Adding react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>React App</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/signup" element={<h1>Signup</h1>} />
+        <Route path="/*" element={<h1>Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
